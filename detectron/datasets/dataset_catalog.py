fcbb3a67dc0ad46b3c941c41b28f7d3a2bf9f522
Northrend <286424061@qq.com>
2018-06-28 19:24:41 +0800

    update dataset_catalog.py

--- detectron/datasets/dataset_catalog.py
+++ detectron/datasets/dataset_catalog.py
@@ -237,21 +237,21 @@ _DATASETS = {
     },
     # oiv4-challenge-2018 coco-style annotations
     'openimage_challenge_2018_train_off': {
-        IM_DIR:
+        _IM_DIR:
             _DATA_DIR + '/openimage/Image',
-        ANN_FN:
+        _ANN_FN:
             _DATA_DIR + '/openimage/annotations/challenge-2018-train-official-annotations-bbox.json'
     },
     'openimage_challenge_2018_train_ext': {
-        IM_DIR:
+        _IM_DIR:
             _DATA_DIR + '/openimage/Image',
-        ANN_FN:
+        _ANN_FN:
             _DATA_DIR + '/openimage/annotations/challenge-2018-train-extended-annotations-bbox.json'
     },
     'openimage_challenge_2018_validation': {
-        IM_DIR:
+        _IM_DIR:
             _DATA_DIR + '/openimage/Image',
-        ANN_FN:
+        _ANN_FN:
             _DATA_DIR + '/openimage/annotations/challenge-2018-validation-annotations-bbox.json'
     },
     # juggdet
